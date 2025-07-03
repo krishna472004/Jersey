36d50ceb17083d7c2bc7a4b64b01166c1a712303
krishna472004 <morthikrishna2017@gmail.com>
2025-07-03 15:03:48 +0530

    Updated demo

---
+++ src/main/java/com/krishna/App7/Controller/Demo.java
@@ -0,0 +1,12 @@
+class Demo
+{
+    public static void main(String[] args) {
+        System.out.println("Hi !");
+        // This is a simple Java application that prints a message to the console.
+        // You can run this application to see the output.
+        for(int i=0;int j=0;i<10;i++,j++) {
+            System.out.println("i: " + i + ", j: " + j);
+        }   
+
+    }                       
+}
