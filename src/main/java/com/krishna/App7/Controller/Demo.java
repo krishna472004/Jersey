8e7469b115dd411407f84ff63e3398e5a936f40c
krishna472004 <morthikrishna2017@gmail.com>
2025-07-03 17:38:17 +0530

    Finale

--- src/main/java/com/krishna/App7/Controller/Demo.java
+++ src/main/java/com/krishna/App7/Controller/Demo.java
@@ -4,4 +4,16 @@ class Demo
     {
         System.out.println("Hello, World!");
     }
+    for(int i = 0; i < 10; i++)
+    {
+        System.out.println("Current number: " + i);
+    }   
+    System.out.println("Loop completed.");
+
+    for(int i = 0; i < 5; i++)
+    {
+        System.out.println("This is a simple loop iteration: " + i);
+    }
+
+
 }
